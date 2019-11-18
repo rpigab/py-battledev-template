40d7bb523b41239f371da54d13a43ed012892a33
rpigab <rpigab@gmail.com>
2019-11-18 13:21:10 +0100

    rename main to solve, add default url to download without parameters

--- script/downloadData.sh
+++ script/downloadData.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-set -eux
+set -ex
 
 DATA_FOLDER=./data
 TMP_ZIP_FILE=/tmp/temp.zip
+ZIP_URL_DEFAULT=https://questionsacm.isograd.com/codecontest/sample_input_output/sample-lJdOuPUVRty107MkPy5Em4OWCFIWHYf6rV_gf6jAgU4.zip
+ZIP_URL_ARG=$1
 
 rm -f "${DATA_FOLDER}"/*.out ${DATA_FOLDER}/*.txt
-wget --no-check-certificate $1 -O "${TMP_ZIP_FILE}"
+wget --no-check-certificate "${ZIP_URL_ARG:-$ZIP_URL_DEFAULT}" -O "${TMP_ZIP_FILE}"
 unzip -j "${TMP_ZIP_FILE}" "*.txt" -d "${DATA_FOLDER}/"
 rm "${TMP_ZIP_FILE}"
