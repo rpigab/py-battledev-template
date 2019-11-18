da33018a9d48620af065a642be4a13acf0f844c3
rpigab <rpigab@gmail.com>
2019-11-18 13:28:48 +0100

    fix script name

--- script/check.sh
+++ script/check.sh
@@ -8,7 +8,7 @@ for f in ./data/input*.txt; do
     num=$(echo "$filename" | grep -o -E '[0-9]+' | head -1 | sed -e 's/^0\+//')
     printf "FILE #%s: %s\n" "${num}" "$f"
     outfile="./data/result${num}.out"
-    ./src/main.py < "$f" > "$outfile" 2>/dev/null
+    ./src/solve.py < "$f" > "$outfile" 2>/dev/null
     diff "$outfile" "./data/output${num}.txt" | sed 's/^/\t/'
     printf "END FILE #%s\n\n" "${num}"
 done
