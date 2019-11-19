724f83f540c5e899c93b2985cbcca0cf02865964
rpigab <rpigab@gmail.com>
2019-11-19 13:34:16 +0100

    feat: check.py produces diff, remove no longer needed bash scripts

--- src/check.py
+++ src/check.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import difflib
 import glob
 import os
 import pathlib
 import re
+import sys
 
 import solve
 
@@ -14,9 +16,18 @@ input_filename_pattern = 'input*.txt'
 result_filename_pattern = 'result{}.out'
 output_filename_pattern = 'output{}.txt'
 
+# Iterate over files in zip
 for filepath in sorted(glob.glob(os.path.join(data_dir, input_filename_pattern))):
     base_filename = os.path.splitext(os.path.basename(filepath))[0]
+    print('Solving with input file {}'.format(base_filename))
     file_number = re.search(r'\d+', base_filename).group()
+    # Solve input<n>.txt and write result to result<n>.out
     with open(filepath, 'r') as input_stream, \
-            open(os.path.join(data_dir, result_filename_pattern.format(file_number)), 'w') as output_stream:
-        solve.solve(input_stream, output_stream)
+            open(os.path.join(data_dir, result_filename_pattern.format(file_number)), 'w') as result_stream:
+        solve.solve(input_stream, result_stream)
+    print('Diff:')
+    # Compare produced result<n>.out with expected data from output<n>.txt
+    with open(os.path.join(data_dir, output_filename_pattern.format(file_number)), 'r') as output_stream, \
+            open(os.path.join(data_dir, result_filename_pattern.format(file_number)), 'r') as result_stream:
+        diff = difflib.context_diff(result_stream.readlines(), output_stream.readlines())
+        sys.stderr.writelines(diff)
--- src/download.py
+++ src/download.py
@@ -17,19 +17,26 @@ zip_filepath = data_dir / 'data.zip'
 url = sys.argv[1] if len(sys.argv) >= 2 else DEFAULT_URL
 
 # Get zip from url
+print('Downloading zip file from {}'.format(url))
 response = requests.get(url)
 if not response:
     exit('Invalid response, got status code {}'.format(response.status_code))
 zip_file = ZipFile(io.BytesIO(requests.get(url).content))
 
 # Delete existing txt files from data folder
+print('Deleting .txt files from data folder')
 txt_files = os.listdir(data_dir)
 for file in txt_files:
     if file.endswith('.txt'):
         os.remove(data_dir / file)
 
 # Extract .txt files to data folder
-for fileName in zip_file.namelist():
-    if fileName.endswith('.txt'):
-        with open(data_dir / pathlib.PurePath(fileName).name, 'wb') as f_out:
-            f_out.write(zip_file.read(fileName))
+print('Extracting individual text files to data folder')
+for file_name in zip_file.namelist():
+    if file_name.endswith('.txt'):
+        base_name = pathlib.PurePath(file_name).name
+        print('Extracting file from archive as {}'.format(base_name))
+        with open(data_dir / base_name, 'wb') as f_out:
+            f_out.write(zip_file.read(file_name))
+    else:
+        print('Ignoring non txt file {}'.format(file_name))
